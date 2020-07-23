d2dcf53940ba484d2a2e8240fe419a4217395e59
Son Nguyen <son.nguyen@rockship.co>
2020-07-23 11:00:12 +0700

    setup base di

---
+++ app/src/main/java/com/sdt/tikihometest/di/ApplicationComponent.java
@@ -0,0 +1,35 @@
+package com.sdt.tikihometest.di;
+
+import android.content.Context;
+
+import com.sdt.tikihometest.App;
+import com.sdt.tikihometest.di.module.AppModule;
+import com.sdt.tikihometest.di.module.NetworkModule;
+import com.sdt.tikihometest.di.module.RepositoriesModule;
+
+import javax.inject.Singleton;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+
+/**
+ * Main component for the application.
+ */
+@Singleton
+@Component(modules = {
+    AndroidSupportInjectionModule.class,
+    AppModule.class,
+    NetworkModule.class,
+    RepositoriesModule.class,
+})
+public interface ApplicationComponent extends AndroidInjector<App> {
+
+    @Component.Factory
+    interface Factory {
+        ApplicationComponent create(@BindsInstance Context applicationContext);
+    }
+
+}
---
+++ app/src/main/java/com/sdt/tikihometest/di/module/RepositoriesModule.java
@@ -0,0 +1,8 @@
+package com.sdt.tikihometest.di.module;
+
+import dagger.Module;
+
+@Module
+public class RepositoriesModule {
+
+}
