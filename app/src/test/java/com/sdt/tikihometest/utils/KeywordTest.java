fb4a4a896b1006db6fc90ef138cef4d676b59076
Son Nguyen <son.nguyen@rockship.co>
2020-07-23 19:32:54 +0700

    change the way of test formatKeyword

--- app/src/test/java/com/sdt/tikihometest/utils/KeywordTest.java
+++ app/src/test/java/com/sdt/tikihometest/utils/KeywordTest.java
@@ -2,31 +2,18 @@ package com.sdt.tikihometest.utils;
 
 import org.junit.Test;
 
+import timber.log.Timber;
+
 import static org.junit.Assert.*;
 
 public class KeywordTest {
 
     @Test
     public void formatKeywords() {
-        String[] keywords = {
-            "bitis hunter",
-            "bitis hunter x",
-            "tai nghe",
-            "harry potter",
-            "balo nữ",
-            "nguyễn nhật ánh",
-            "đắc nhân tâm",
-            "tai nghe bluetooth",
-            "kem chống nắng",
-            "anh chính là thanh xuân của em",
-            "trung tâm kiểm soát"
-        };
-
-        for (String keyword : keywords) {
-            String newKeyword = BindingUtils.formatKeyword(keyword);
-            // It's true if newKeyword != keyword
-            assertNotEquals(newKeyword, keyword);
-        }
+        String keyword = "nguyễn nhật ánh";
+        String expected = "nguyễn\nnhật ánh";
+        String newKeyword = BindingUtils.formatKeyword(keyword);
+        assertEquals(newKeyword, expected);
     }
 
 }
